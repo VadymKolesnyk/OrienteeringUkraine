aef24f400f90c1475c8caa5efaeda394f12fc501
VadymKolesnyk <vakol2001@gmail.com>
2020-12-09 15:01:57 +0200

    Add controlers branch and test DataManager

---
+++ OrienteeringUkraine/TempDataManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrienteeringUkraine
+{
+    public class TempDataManager : IDataManager // Потом удалить (тестовый DataManager)
+    {
+    }
+}
