5724895a1a4fbfdb14a7ec6e3112eed5d3532257
sergey-judi <serega7qwerty@gmail.com>
2020-12-13 12:45:09 +0200

    Add Logins

--- OrienteeringUkraine/Startup.cs
+++ OrienteeringUkraine/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace OrienteeringUkraine
 {
@@ -642,8 +643,82 @@ namespace OrienteeringUkraine
                         );
                 }
 
+                if (!context.Logins.Any())
+                {
+                    for (int i = 1; i < context.Users.Count<User>(); i++)
+                    {
+                        string login = "sportsman" + i.ToString();
+                        if (i == 30)
+                            login = "admin";
+                        else if (i == 31 || i == 32)
+                            login = "moderator" + (i % 10).ToString();
+                        else if (i >= 33 && i <= 36)
+                            login = "organizer" + (i % 10).ToString();
+                        int id = i;
+                        string hashedPassword = HashPassword(login);
+                        context.Logins.Add(new LoginData { Login = login, UserId = id, HashedPassword = hashedPassword });
+                    }
+                }
+
                 context.SaveChanges();
             }
+        }
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            return ByteArraysEqual(buffer3, buffer4);
+        }
+
+        public static bool ByteArraysEqual(byte[] b1, byte[] b2)
+        {
+            if (b1 == b2) return true;
+            if (b1 == null || b2 == null) return false;
+            if (b1.Length != b2.Length) return false;
+            for (int i = 0; i < b1.Length; i++)
+            {
+                if (b1[i] != b2[i]) return false;
+            }
+            return true;
         }
     }
 }
