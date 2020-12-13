29df6c8ddcf0e7cc40c0cd69683d095ba6ad01d5
sergey-judi <serega7qwerty@gmail.com>
2020-12-13 11:50:48 +0200

    Edit Regions

---
+++ OrienteeringUkraine/Migrations/20201213094851_Remove-Regions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrienteeringUkraine.Migrations
+{
+    public partial class RemoveRegions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+        }
+    }
+}
