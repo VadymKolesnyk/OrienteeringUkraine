dab9d64fc4fb79a8177a29c9c4f6ff9700000fae
KaterynaKub <ekaterina.kubyshka@gmail.com>
2020-12-16 22:53:02 +0200

    Tests for HomeController

---
+++ HomeController.UnitTests/HomeControllerUnitTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrienteeringUkraine.Controllers;
+
+namespace Home.UnitTests
+{
+    [TestClass]
+    public class HomeControllerUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            OrienteeringUkraine.Controllers.HomeController home = 
+        }
+    }
+}
---
+++ UnitTests/HomeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrienteeringUkraine;
+using OrienteeringUkraine.Controllers;
+using OrienteeringUkraine.Data;
+using System.Web;
+using Moq;
+using System.Collections.Generic;
+using OrienteeringUkraine.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HomeTests
+    {
+        private IDataManager manager = new TempDataManager();
+        private HomeIndexData data = new HomeIndexData();
+
+        [TestMethod]
+        public void HomeModelIsNotNull()
+        {
+            HomeController homeController = new HomeController(manager);
+            var result = homeController.Index(data) as ViewResult;
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void DbReturnsNullTest() // positive test
+        {
+            var mock = new Mock<IDataManager>();
+            HomeController controller = new HomeController(mock.Object);
+            var result = controller.Index(data);
+            Assert.IsNotNull(result);
+        }
+    }
+}
